31c4a053d13bc63cdad64abecfc180609e115f4c
ETMANZI <eugenemanzi03@gmail.com>
2026-03-26 14:19:52 +0200

    Build frontend into Django static

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "/static/frontend/",
+  build: {
+    outDir: path.resolve(__dirname, "../backend/static/frontend"),
+    emptyOutDir: true,
+  },
 });
+
+
+
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   base: "/static/frontend/",
+// });

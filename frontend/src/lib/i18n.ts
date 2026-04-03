2aea69ddb34b596504a1df7e86b73ee9c8e9f303
ETMANZI <eugenemanzi03@gmail.com>
2026-04-03 19:35:00 +0200

    Bundle translations directly into frontend

--- frontend/src/lib/i18n.ts
+++ frontend/src/lib/i18n.ts
@@ -1,18 +1,21 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import HttpBackend from 'i18next-http-backend';
+
+import enTranslation from '../locales/en/translation.json';
+import rwTranslation from '../locales/rw/translation.json';
 
 const savedLanguage = localStorage.getItem('preferred_language') || 'en';
-const version = '2026-04-03-1'; // change this whenever you update translations
 
 i18n
-  .use(HttpBackend)
   .use(initReactI18next)
   .init({
-    backend: {
-      loadPath: import.meta.env.DEV
-        ? `/locales/{{lng}}/translation.json?v=${version}`
-        : `/static/frontend/locales/{{lng}}/translation.json?v=${version}`,
+    resources: {
+      en: {
+        translation: enTranslation,
+      },
+      rw: {
+        translation: rwTranslation,
+      },
     },
     lng: savedLanguage,
     fallbackLng: 'en',

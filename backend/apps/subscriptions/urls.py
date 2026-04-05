e7632052a48e782ce10e8c02d67e9b3ad8328ce0
ETMANZI <eugenemanzi03@gmail.com>
2026-04-05 20:15:41 +0200

    Fix C24

--- backend/apps/subscriptions/urls.py
+++ backend/apps/subscriptions/urls.py
@@ -8,6 +8,7 @@ from .views import (
     AdminSubscriptionPaymentListView,
     AdminApproveSubscriptionView,
     AdminRejectSubscriptionView,
+    UserSubscriptionLimitsView,
 )
 
 urlpatterns = [
@@ -15,7 +16,7 @@ urlpatterns = [
     path("my-subscription/", MySubscriptionView.as_view(), name="my-subscription"),
     path("subscribe/", SubscribeToPlanView.as_view(), name="subscribe-to-plan"),
     path("my-payments/", MySubscriptionPaymentsView.as_view(), name="my-subscription-payments"),
-
+    path("my-subscription-limits/", UserSubscriptionLimitsView.as_view(), name="subscription-limits"),
     path("admin/requests/", AdminSubscriptionRequestListView.as_view(), name="admin-subscription-requests"),
     path("admin/payments/", AdminSubscriptionPaymentListView.as_view(), name="admin-subscription-payments"),
     path("admin/requests/<int:pk>/approve/", AdminApproveSubscriptionView.as_view(), name="admin-approve-subscription"),

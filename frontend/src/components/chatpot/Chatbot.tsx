1a82186ee284e8d8fb23adcdc0a22a5f0ac6aa1a
ETMANZI <eugenemanzi03@gmail.com>
2026-04-11 11:59:36 +0200

    A FIXA 3

--- frontend/src/components/chatpot/Chatbot.tsx
+++ frontend/src/components/chatpot/Chatbot.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from 'react';
-// import { useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import { 
   MessageCircle, 
   X, 
@@ -61,7 +61,7 @@ const SUGGESTED_TEXTS = {
 };
 
 export default function Chatbot() {
-  // const { t } = useTranslation();
+  const { t } = useTranslation();
   const [isOpen, setIsOpen] = useState(false);
   const [isMinimized, setIsMinimized] = useState(false);
   const [messages, setMessages] = useState<Message[]>([]);

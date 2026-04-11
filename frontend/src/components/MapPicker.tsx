5657c976b9ae24128e036ee38d1a86d2316114a2
ETMANZI <eugenemanzi03@gmail.com>
2026-04-11 18:21:40 +0200

    A FIXA 16d

--- frontend/src/components/MapPicker.tsx
+++ frontend/src/components/MapPicker.tsx
@@ -186,6 +186,11 @@ export default function MapPicker({ latitude, longitude, onChange }: Props) {
           url="https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
         />
         
+      {/* <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a> contributors'
+        url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+      /> */}
+
         {/* You can also try this hybrid view (satellite + labels):
         <TileLayer
           attribution='&copy; <a href="https://www.google.com/maps">Google Maps</a>'
